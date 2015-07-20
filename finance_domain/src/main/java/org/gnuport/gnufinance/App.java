9e2a1ce4b73c48300007893844f71f4baf1fa194
Suresh Sarda <suresh.sarda@infibeam.net>
2015-07-20 18:18:03 +0530

    Projects Created

---
+++ finance_domain/src/main/java/org/gnuport/gnufinance/App.java
@@ -0,0 +1,13 @@
+package org.gnuport.gnufinance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
