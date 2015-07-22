b617ca6dd9539757b256d804e634f56a0bbce4a5
Suresh Sarda <suresh.sarda@infibeam.net>
2015-07-22 23:10:21 +0530

    Added getters and setters

--- gnu_finance/src/main/java/org/gnuport/gnufinance/beans/UserBean.java
+++ gnu_finance/src/main/java/org/gnuport/gnufinance/beans/UserBean.java
@@ -3,6 +3,7 @@ package org.gnuport.gnufinance.beans;
 import java.util.Date;
 
 public class UserBean {
+	long rid;
 	String firstName;
 	String lastName;
 	Date dob;
@@ -15,5 +16,93 @@ public class UserBean {
 	String country;
 	
 	String dataDirectory;
+
+	public long getRid() {
+		return rid;
+	}
+
+	public void setRid(long rid) {
+		this.rid = rid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getDataDirectory() {
+		return dataDirectory;
+	}
+
+	public void setDataDirectory(String dataDirectory) {
+		this.dataDirectory = dataDirectory;
+	}
 	
 }
