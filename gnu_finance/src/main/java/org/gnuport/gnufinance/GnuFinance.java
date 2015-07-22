b774f8bc444940c42a7c9eb510cf2333bee9bc0b
Suresh Sarda <suresh.sarda@infibeam.net>
2015-07-22 19:38:07 +0530

    Added and configured logger

--- gnu_finance/src/main/java/org/gnuport/gnufinance/GnuFinance.java
+++ gnu_finance/src/main/java/org/gnuport/gnufinance/GnuFinance.java
@@ -1,11 +1,18 @@
 package org.gnuport.gnufinance;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GnuFinance {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public static void main(String[] args) {
-		
-		
-		
-		
+		new GnuFinance();
+	}
+
+	public GnuFinance() {
+		logger.debug("Log");
+
 	}
 }
