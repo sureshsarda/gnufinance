f62e0fced09ecf14612eb9d3f6d0413a0e82500b
Suresh Sarda <suresh.sarda@infibeam.net>
2015-07-23 12:37:52 +0530

    Methods added

--- gnu_finance/src/main/java/org/gnuport/gnufinance/utils/NamedPreparedStatement.java
+++ gnu_finance/src/main/java/org/gnuport/gnufinance/utils/NamedPreparedStatement.java
@@ -2,10 +2,16 @@ package org.gnuport.gnufinance.utils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.log4j.lf5.util.DateFormatManager;
+
 /**
- *
+ * NamedPreparedStatement helps to create SQL statements using symbols instead
+ * of indexes as in vanilla prepared statement. A symbol is prefixed by a ':'
+ * (colon). All the symbols must be replaced by their values before executing.
+ * 
  * @author Suresh Sarda
  */
 
@@ -30,12 +36,45 @@ public class NamedPreparedStatement {
 		setString(symbol, val);
 	}
 
+	public void setDate(String symbol, java.util.Date date) {
+		DateFormatManager dfm = new DateFormatManager("yyyy-MM-dd");
+		String strDate = dfm.format(date);
+		setString(symbol, strDate);
+	}
+
+	public void setDate(String symbol, java.sql.Date date) {
+		DateFormatManager dfm = new DateFormatManager("yyyy-MM-dd");
+		String strDate = dfm.format(date);
+		setString(symbol, strDate);
+	}
+
+	/*
+	 * Return Objects
+	 */
 	public PreparedStatement getPreparedStatement() throws SQLException {
 		return connection.prepareStatement(query);
 	}
 
 	public String getSqlQuery() {
 		return query;
+	}
+	
+	/*
+	 * Statement Execution Routines
+	 */
+	public boolean execute() throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(query);
+		return ps.execute();
+	}
+	
+	public ResultSet executeQuery() throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(query);
+		return ps.executeQuery();
+	}
+	
+	public int executeUpdate() throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(query);
+		return ps.executeUpdate();
 	}
 
 }
